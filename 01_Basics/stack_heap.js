61182c4df0e327d38f8100ee2d558c90f82e0d30
omtiwari52 <tiwariom900@gmail.com>
2024-02-10 22:13:55 +0530

    Project Done

---
+++ 01_Basics/stack_heap.js
@@ -0,0 +1,26 @@
+// Stack (Primitive)
+// Heap (Non-Primitve)
+
+
+// Stack //
+
+// let myutubeName = "KuchBhi"
+//  let anotherName = myutubeName
+//  anotherName = "tiwariom"
+
+//  console.log(myutubeName)
+//  console.log(anotherName)
+
+// Heap // 
+
+// let userOne = {
+//     email : "user@google.com" ,
+//     upi : "user@ybl"
+// }
+
+// let userTwo = userOne
+
+// userTwo.email = "microsoft@gmail.com"
+
+// console.log(userOne)
+// console.log(userTwo)
